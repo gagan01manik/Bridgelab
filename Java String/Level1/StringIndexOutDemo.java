dd0a572de71ef71618d68a14be2898e6c59fdec2
gagan01manik <gaganmanik4@gmail.com>
2025-08-11 18:30:17 +0530

    code push

---
+++ Java String/Level1/StringIndexOutDemo.java
@@ -0,0 +1,27 @@
+package Java String.Level1;
+
+public class StringIndexOutDemo {
+    
+}
+import java.util.*;
+
+public class StringIndexOutDemo {
+    static void throwException(String str) {
+        System.out.println(str.charAt(str.length())); 
+    }
+
+    static void handleException(String str) {
+        try {
+            System.out.println(str.charAt(str.length()));
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Handled StringIndexOutOfBoundsException");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+     
+        handleException(s);
+    }
+}
